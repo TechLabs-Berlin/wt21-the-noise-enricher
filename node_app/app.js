540028ed64a49a03d379f767f2f35b948a0b3d51
vagechirkov <vagechirkov@gmail.com>
2021-12-11 12:55:24 +0100

    add basic layout and boilerplate

--- node_app/app.js
+++ node_app/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 const path = require('path');
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.render('home');
