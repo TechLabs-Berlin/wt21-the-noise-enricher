3e34a85848e2108dc3ccb1d10db99b8ded6b6f68
vagechirkov <vagechirkov@gmail.com>
2021-12-11 08:00:30 +0100

    add basic node package structure

---
+++ node_app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+
+app.listen(3030, () => {
+    console.log("Serving on port 3030")
+})
