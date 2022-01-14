1c235f6d9151dde56a7fbd51d97634d721862236
vagechirkov <vagechirkov@gmail.com>
2022-01-15 00:12:27 +0100

    add templates

--- node_app/server/app.js
+++ node_app/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const bodyParser = require("body-parser");
+const ejsMate = require('ejs-mate');
 const app = express();
 const router = express.Router();
 const morgan = require('morgan');
@@ -8,16 +9,25 @@ const path = require('path');
 
 const upload = multer({ dest: 'uploads/' })
 
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/../client/views'));
+
 app.use(express.static(path.join(__dirname, '/../client')));
+
 app.use('/',router);
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 
 router.route('/')
+    .get((req, res) => {
+        res.render('home');
+    })
     .post(upload.single('audio'), (req, res) => {
-    console.log(req.file, req.body);
-    res.redirect('/');
+        console.log(req.file, req.body);
+        res.redirect(303, '/');
+        // res.status(301)
 })
 
 const port = process.env.PORT || 3030;
