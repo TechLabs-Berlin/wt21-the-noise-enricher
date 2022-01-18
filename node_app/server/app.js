ae1816df2814b7719e20c1dfa9552ae2cbdf66d3
vagechirkov <vagechirkov@gmail.com>
2022-01-18 10:19:36 +0100

    update routes and templates

--- node_app/server/app.js
+++ node_app/server/app.js
@@ -1,34 +1,40 @@
 const express = require('express');
-const multer = require('multer');
 const bodyParser = require("body-parser");
 const ejsMate = require('ejs-mate');
 const app = express();
-const router = express.Router();
+const generateRoutes = require('./routes/generate');
 const morgan = require('morgan');
 const path = require('path');
 
-const upload = multer({ dest: 'uploads/' })
+// const session = require('express-session');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/../client/views'));
-
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client')));
-
-app.use('/',router);
 app.use(morgan('dev'));
 
-app.use(bodyParser.json());
 
-router.route('/')
-    .get((req, res) => {
-        res.render('home');
-    })
-    .post(upload.single('audio'), (req, res) => {
-        console.log(req.file, req.body);
-        res.redirect(303, '/');
-        // res.status(301)
-})
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+app.use('/generate', generateRoutes);
+
+
+
+// router.route('/')
+//     .get((req, res) => {
+//         res.render('home');
+//     })
+//     .post(upload.single('audio'), (req, res) => {
+//         console.log(req.file, req.body);
+//         res.redirect(303, '/');
+//         // res.status(301)
+// })
+
+
 
 const port = process.env.PORT || 3030;
 
---
+++ node_app/server/routes/generate.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' })
+
+router.get('/', (req, res) => {
+    res.render('generate/index');
+});
+
+router.post('/', upload.single('audio'), (req, res) => {
+    console.log(req.file, req.body);
+    res.redirect(303, 'generate/spectrogram');
+});
+
+router.get('/spectrogram', (req, res) => {
+    res.render('generate/spectrogram');
+});
+
+router.get('/results', (req, res) => {
+    res.render('generate/results');
+});
+
+module.exports = router;
+
