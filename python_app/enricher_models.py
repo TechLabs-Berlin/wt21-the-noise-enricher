9c985f96c774d65832b3e0fe4838e49791ae392a
vagechirkov <vagechirkov@gmail.com>
2022-01-27 11:48:31 +0100

    comment CNN for now

--- python_app/enricher_models.py
+++ python_app/enricher_models.py
@@ -20,28 +20,28 @@ except:
 tf.compat.v1.disable_eager_execution()
 
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-
-        self.cnn_layers = nn.Sequential(
-            nn.Conv2d(2 ,  16, kernel_size=7, stride=1), nn.Tanh(), nn.MaxPool2d(2),
-            nn.Conv2d(16,  32, kernel_size=5, stride=1), nn.Tanh(), nn.MaxPool2d(2),
-            nn.Conv2d(32,  64, kernel_size=6, stride=1), nn.Tanh(), nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, kernel_size=5, stride=1), nn.Tanh(), nn.MaxPool2d(2),
-            nn.Flatten()
-        )
-
-        self.linear_layers = nn.Sequential(
-            nn.Linear(12*12*128, 2048),
-            nn.Linear(2048, 10)
-        )
-
-    def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
+# class CNN(nn.Module):
+#     def __init__(self):
+#         super(CNN, self).__init__()
+#
+#         self.cnn_layers = nn.Sequential(
+#             nn.Conv2d(2 ,  16, kernel_size=7, stride=1), nn.Tanh(), nn.MaxPool2d(2),
+#             nn.Conv2d(16,  32, kernel_size=5, stride=1), nn.Tanh(), nn.MaxPool2d(2),
+#             nn.Conv2d(32,  64, kernel_size=6, stride=1), nn.Tanh(), nn.MaxPool2d(2),
+#             nn.Conv2d(64, 128, kernel_size=5, stride=1), nn.Tanh(), nn.MaxPool2d(2),
+#             nn.Flatten()
+#         )
+#
+#         self.linear_layers = nn.Sequential(
+#             nn.Linear(12*12*128, 2048),
+#             nn.Linear(2048, 10)
+#         )
+#
+#     def forward(self, x):
+#         x = self.cnn_layers(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.linear_layers(x)
+#         return x
         
 
 
