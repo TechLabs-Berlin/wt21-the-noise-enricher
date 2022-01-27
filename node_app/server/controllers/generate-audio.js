1ba27b1ae8dea276c5c0623911a9043bbcf9f56f
vagechirkov <vagechirkov@gmail.com>
2022-01-27 15:57:35 +0100

    add ajax request

---
+++ node_app/server/controllers/generate-audio.js
@@ -0,0 +1,69 @@
+const runPython = require("./run-python");
+const path = require("path");
+const fs = require("fs");
+
+const audioFile = {};
+let generationDone = false;
+
+module.exports.index = (req, res) => {
+    res.render('index');
+
+    // clean upload folder
+    const directory = path.join(__dirname, '../../client/public/uploads/');
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+}
+
+
+module.exports.showSpectrogram = async (req, res) => {
+    if (audioFile.file) {
+        res.render('generate/spectrogram', {filepath: audioFile.file.filename});
+
+        const code = await runPython.runNoiseEnricher(audioFile.file.path, audioFile.file.filename);
+
+        if (code === 0) {
+            generationDone = true;
+        }
+    }
+    else
+        res.redirect(302, 'generate');
+
+};
+
+
+
+module.exports.checkResultsStatus = async (req, res) => {
+    if(audioFile.file && generationDone) {
+        res.send({status: 'done'});
+    }
+    else
+        res.send({status: 'not_done'});
+};
+
+module.exports.computeResults = async (req, res) => {
+    if(audioFile.file && generationDone) {
+        const generated_file = '../../public/uploads/reconstructed_' + audioFile.file.filename + "_0.wav";
+
+        res.render('generate/results', {filepath: generated_file});
+        generationDone = false;
+    } else {
+        res.redirect(302, 'generate');
+    }
+};
+
+
+module.exports.uploadForm = async (req, res) => {
+    if (req.file) {
+        audioFile.file = req.file;
+        res.redirect(302, 'generate/spectrogram');
+    }
+    else
+        res.redirect(302, 'generate');
+}
--- node_app/server/routes/generate.js
+++ node_app/server/routes/generate.js
@@ -3,73 +3,29 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require("path");
-const runPython = require("../controllers/run-python")
+const audioGenerate = require("../controllers/generate-audio")
+const catchAsync = require('../utils/catchAsync');
 
 
-
-const audioFile = {};
-
-// const limits = { fileSize: 1024 * 1024 * 1024 }
 const upload = multer({
-    dest: path.join(__dirname, '../../client/public/uploads/'),
-    // limits: limits
+    dest: path.join(__dirname, '../../client/public/uploads/')
 });
 
-router.get('/', (req, res) => {
-    res.render('index');
-
-    // clean upload folder
-   const directory = path.join(__dirname, '../../client/public/uploads/');
-    fs.readdir(directory, (err, files) => {
-        if (err) throw err;
-
-        for (const file of files) {
-            fs.unlink(path.join(directory, file), err => {
-                if (err) throw err;
-            });
-        }
-    });
-});
+router.get('/', audioGenerate.index);
 
 router.get('/pro', (req, res) => {
     res.render('generate/generate_pro');
 });
 
-router.post('/', upload.single('audio'), async (req, res) => {
-    if (req.file) {
-        audioFile.file = req.file;
-        res.redirect(302, 'generate/results');
-    }
-    else
-        res.redirect(302, 'generate');
-});
+router.post('/', upload.single('audio'), audioGenerate.uploadForm);
 
-router.get('/spectrogram', async (req, res) => {
-    if (audioFile.file) {
-        res.render('generate/spectrogram', {filepath: audioFile.file.filename});
+router.get('/spectrogram', audioGenerate.showSpectrogram);
 
-    }
-    else
-        res.redirect(302, 'generate');
-});
+router.post('/check-results', audioGenerate.checkResultsStatus);
 
-router.get('/results', async (req, res) => {
-    if(audioFile.file) {
-        const code = await runPython.runNoiseEnricher(audioFile.file.path, audioFile.file.filename);
-
-        if (code !== 0) {
-            res.redirect(302, 'generate');
-        }
-
-        const generated_file = '../../public/uploads/reconstructed_' + audioFile.file.filename + "_0.wav";
+router.get('/results', audioGenerate.computeResults);
 
 
-        res.render('generate/results', { filepath: generated_file});
-    } else {
-        res.redirect(302, 'generate');
-    }
-
-});
 
 module.exports = router;
 
